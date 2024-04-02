ad0df7470cbe6d897c3eb0e6305dee99b4cfadc7
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-04-02 15:39:28 -0500

    Actualizar middleware de manejo de errores

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.use('/reminders', reminderRoutes);
 app.use('/users', userRoutes);
 
 // Error handling middleware should be placed after the routes
-app.use((req, res, next) => {
+app.use((_req, _res, next) => {
     const error = new Error("Not found");
     error.status = 404;
     next(error);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     res.json({
         error: {
