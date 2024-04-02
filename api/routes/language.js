6836d48005b90112a5d4978cfa682b8f451e8a66
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-04-02 15:39:18 -0500

    Actualizar ruta de idioma para obtener idioma por ID de gremio

--- api/routes/language.js
+++ api/routes/language.js
@@ -7,35 +7,34 @@ const Config = require("../models/config");
 router.use(express.json());
 
 /*
-* / GET Language by the Guild ID
-*/
+ * / GET Language by the Guild ID
+ */
 
 router.get("/", async (req, res, next) => {
-    if (!req.body["guildID"]) {
-        let newData = {
-            code: 200,
-            language: req.app.locals["en"]
-        }
-        res.send(newData);
-        return;
-    }
+  if (!req.query["guildID"]) {
+    let newData = {
+      code: 200,
+      language: req.app.locals["en"],
+    };
+    res.send(newData);
+    return;
+  }
 
-    try {
-        const data = await Config.findOne({ guildID: req.body["guildID"] });
+  try {
+    const data = await Config.findOne({ guildID: req.query["guildID"] });
 
-        if (!data) return res.status(404).send({ error: "Guild not found." });
+    if (!data) return res.status(404).send({ error: "Guild not found." });
 
-        let dataToSend = req.app.locals[data.language];
-        let newData = {
-            code: 200,
-            guildID: data.guildID,
-            language: dataToSend
-        }
-        res.send(newData);
-    } catch (err) {
-        res.status(500).send({ error: "Internal server error." });
-    }
+    let dataToSend = req.app.locals[data.language];
+    let newData = {
+      code: 200,
+      guildID: data.guildID,
+      language: dataToSend,
+    };
+    res.send(newData);
+  } catch (err) {
+    res.status(500).send({ error: "Internal server error." });
+  }
 });
-
 
 module.exports = router;
